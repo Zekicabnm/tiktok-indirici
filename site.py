253dff22496d4a9e9fed3ffc0b349711c0517945
Zekicabnm <130228022+Zekicabnm@users.noreply.github.com>
2026-01-16 01:44:25 +0300

    Add files via upload

--- site.py
+++ site.py
@@ -132,4 +132,6 @@ def indir():
     return send_file(final, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Port ayarı sunucular için kritiktir
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
